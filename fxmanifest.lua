ecf5db8511b4ae987478f9f82bebddb77e47d6ad
R1CKY6 <100082886+R1CKY6@users.noreply.github.com>
2023-09-25 17:43:26 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,33 @@
+fx_version 'bodacious'
+lua54 'yes' 
+game 'gta5' 
+
+author 'R1CKY®#2220'
+description 'Shop System'
+version '1.0'
+
+
+client_scripts {
+    'client.lua'
+}
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua',
+}
+
+server_scripts {
+--    '@mysql-async/lib/MySQL.lua',
+    'server.lua',
+}
+
+ui_page 'web/index.html'
+
+files {
+    'web/*.html',
+    'web/css/*.css',
+    'web/js/*.js',
+    'web/fonts/*.otf',
+    'web/img/*.png',
+    'web/itemImage/*.png',
+}
